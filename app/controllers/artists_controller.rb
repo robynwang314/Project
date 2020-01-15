543635dff1abb043cfb79e478e875131453ff72d
Robyn Wang <tyrobynwang@gmail.com>
2020-01-15 01:15:58 +0000

    redirects user to root in artist show if not logged in

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,13 +8,17 @@ def index
     redirect_to new_artist_path
   end
 end
-
+ 
 def show
-  u = User.find_by_email(session[:active_email])
-  @user_hash = u.spotify_hash
-  @spotify_user = RSpotify::User.new(u.spotify_hash)
-  @playlists = @spotify_user.playlists
-  @artist = RSpotify::Artist.find(params[:id])
+  if
+    u = User.find_by_email(session[:active_email])
+    @user_hash = u.spotify_hash
+    @spotify_user = RSpotify::User.new(u.spotify_hash)
+    @playlists = @spotify_user.playlists
+    @artist = RSpotify::Artist.find(params[:id])
+  else
+      redirect_to root_path and return
+  end
 end
 
 def new
